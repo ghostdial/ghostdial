99b925bc18b5badbb7fdf49898035f9c7fcdb78e
ghostdial <ghostdialfoundation@gmail.com>
2024-03-21 18:58:28 +0000

    update

--- services/asterisk/docker-entrypoint.sh
+++ services/asterisk/docker-entrypoint.sh
@@ -9,21 +9,21 @@ ASTERISK_USER="${ASTERISK_USER:-asterisk}"
 ASTERISK_GROUP="${ASTERISK_GROUP:-${ASTERISK_USER}}"
 
 if [ "$1" = "" ]; then
-  COMMAND="/usr/sbin/asterisk -T -W -U ${ASTERISK_USER} -p -vvvdddf"
+  COMMAND="/usr/sbin/asterisk -T -U "$ASTERISK_USER" -W -p -vvvdddf"
 else
   COMMAND="$@"
 fi
 
 function write_sip_conf() {
-  IPV4_ADDRESS=$(ip -4 addr show eth0 | grep --color=never -oP '(?<=inet\s)\d+(\.\d+){3}')
+  export IPV4_ADDRESS=$(ip -4 addr show eth0 | grep --color=never -oP '(?<=inet\s)\d+(\.\d+){3}')
   cat /templates/sip.conf.tpl | envsubst > /etc/asterisk/sip.conf
 }
 function echo_env() {
   echo "EXTERN_IP: $EXTERN_IP"
-  echo "VOIPMS_SMS_PROTOCOL: $VOIPMS_SMS_PROTOCOL"
+  echo "VOIPMS_SIP_PROTOCOL: $VOIPMS_SIP_PROTOCOL"
   echo "VOIPMS_SIP_USERNAME: $VOIPMS_SIP_USERNAME"
-  local rewrite=$(echo ${VOIPMS_SIP_PASSWORD} | sed -e 's/./x/g)')
-  echo "VOIPMS_SIP_PASSWORD: $(echo ${VOIPMS_SIP_PASSWORD} | sed -e 's/./x/g)')"
+  local rewrite=$(echo ${VOIPMS_SIP_PASSWORD} | sed -e 's/./x/g')
+  echo "VOIPMS_SIP_PASSWORD: $rewrite"
   echo "VOIPMS_SIP_HOST: $VOIPMS_SIP_HOST"
   echo "VOIPMS_SIP_PORT: $VOIPMS_SIP_PORT"
   echo "TWILIO_ACCOUNT_SID: $TWILIO_ACCOUNT_SID"
@@ -42,24 +42,36 @@ function init_asterisk() {
   fi
 }
 
+function create_user() {
+  adduser --gecos "" --disabled-login --disabled-password --uid "${ASTERISK_UID}" "${ASTERISK_USER}"
+  adduser asterisk asterisk
+}
+
 function include_conf() {
   cd /config
-  for file in /config/*.conf; do
+  for file in *.conf; do
     if [[ ! -f "/etc/asterisk/$file" ]]; then
       cp -v "/config/$file" "/etc/asterisk/$file"
     fi
   done
 }
 
-if [[ "${ASTERISK_UID}" != "" && "${ASTERISK_GID}" != "" ]]; then
-  # recreate user and group for asterisk
-  # if they've sent as env variables (i.e. to macth with host user to fix permissions for mounted folders
-  deluser asterisk && \
-  addgroup -g "${ASTERISK_GID}" "${ASTERISK_GROUP}" && \
-  adduser -D -H -u "${ASTERISK_UID}" -G "${ASTERISK_GROUP}" "${ASTERISK_USER}"
+echo "ASTERISK_UID: $ASTERISK_UID"
+echo "ASTERISK_GID: $ASTERISK_GID"
+
+CURRENT_ASTERISK_ID=$(id -u asterisk)
+echo "CURRENT_ASTERISK_ID: $CURRENT_ASTERISK_ID"
+if [[ ! -z "$CURRENT_ASTERISK_ID"  && $CURRENT_ASTERISK_ID != $ASTERISK_UID ]]; then
+  deluser asterisk
+  create_user
 fi
 
-chown -R "${ASTERISK_USER}": /var/log/asterisk \
+if [[ -z "$CURRENT_ASTERISK_ID" ]]; then
+  create_user
+fi
+
+
+chown -R $ASTERISK_USER /var/log/asterisk \
                            /var/lib/asterisk \
                            /var/run/asterisk \
                            /var/spool/asterisk ; \
